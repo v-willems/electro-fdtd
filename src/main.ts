74037027610897d92cc9c418a12bea023d97b7e8
Victor Willems <victor.willems@t-online.de>
2026-06-29 14:05:57 +0200

    FIX: removed unused charts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { Visualizer } from "./Visualizer.ts";
 import { Simulator } from "./Simulator.ts";
 import {
   Chart,
-  type ChartConfiguration,
-  type ScatterDataPoint,
   LineController,
   LineElement,
   PointElement,
@@ -27,8 +25,6 @@ const sim_canvas = document.getElementById("grid") as HTMLCanvasElement;
 const frameCounter = document.getElementById("framecounter") as HTMLDivElement;
 const sim_ctx = sim_canvas.getContext("2d");
 if (!sim_ctx) throw new Error("2D context not available");
-
-const chart_canvas = document.getElementById("chart") as HTMLCanvasElement;
 
 const height = sim_canvas.height;
 const width = sim_canvas.width;
@@ -58,47 +54,6 @@ console.log(`aHx ${sim.aHx[sim.idx_g(50, 50)]}`);
 console.log(`bHx ${sim.bHx[sim.idx_g(50, 50)]}`);
 console.log(`cHx ${sim.cHx[sim.idx_g(50, 50)]}`);
 sim.runSimulation();
-
-const { freqs, mags } = sim.getEzStatsFrequencies(40, 50);
-
-const points: ScatterDataPoint[] = Array.from(freqs).map((f, idx) => ({
-  x: f,
-  y: mags[idx],
-}));
-
-const config: ChartConfiguration<"line", ScatterDataPoint[], number> = {
-  type: "line",
-  data: {
-    datasets: [
-      {
-        label: "FFT magnitude",
-        data: points,
-        parsing: false as const, // <- key change
-        pointRadius: 0,
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    animation: false,
-    scales: {
-      x: {
-        type: "linear",
-        title: { display: true, text: "Frequency (Hz)" },
-        ticks: {
-          callback: (value) => (Number(value) / 1e9).toString(),
-        },
-        max: 30e9,
-        min: 0,
-      },
-      y: {
-        title: { display: true, text: "Magnitude" },
-      },
-    },
-  },
-};
-
-const chart = new Chart(chart_canvas, config);
 
 const viz = new Visualizer(sim_ctx, {
   x0: 0,
